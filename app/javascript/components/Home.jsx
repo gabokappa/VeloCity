72186c04b9382556b72e17fba76c0ca212f7a24e
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-30 11:28:00 +0000

    Setup

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+    <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
+        <div className="jumbotron jumbotron-fluid bg-transparent">
+            <div className="container secondary-color">
+                <h1 className="display-4">VeloCity the new way to ride</h1>
+                <p className="lead">
+                    Track your bike components with ease to avoid accidents.
+                </p>
+                <hr className="my-4" />
+                <Link
+                    to="/authenticate"
+                    className="btn btn-lg custom-button"
+                    role="button"
+                >
+                    Connect to Strava
+                </Link>
+            </div>
+        </div>
+    </div>
+);
