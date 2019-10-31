9da30cb0f1b4ef84892aefb421d0aa35d17bf551
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-31 14:14:43 +0000

    Test user controller returns and object with id 1

--- spec/api_users_controller_spec.rb
+++ spec/api_users_controller_spec.rb
@@ -9,5 +9,18 @@ RSpec.describe Api::V1::UsersController, type: :request do
       get '/api/v1/users/'
       expect(response).to have_http_status(200)
     end
+
+    it 'returns a JSON' do
+      get '/api/v1/users/index/'
+      json = JSON.parse(response.body)
+      expect(json.length).to eq(User.count)
+    end
+
+    it 'first user has an id of 1' do
+      get '/api/v1/users/index/'
+      json = JSON.parse(response.body)
+      expect(json.first['id']).to eq(1)
+    end
+
   end
 end
