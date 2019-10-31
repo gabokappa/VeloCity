4e5f2609a26a7fb4ee5c1c63fe56d2c14536b582
James Holton <james_holton@yahoo.co.uk>
2019-10-31 15:50:57 +0000

    Final small format layout changes

--- app/javascript/components/StravaAuth.js
+++ app/javascript/components/StravaAuth.js
@@ -20,10 +20,12 @@ class StravaAuth extends Component {
 
   render() {
     return (
-      <div>
+
+      <div className="container py-1">
         <h1 className="display-4">Click <a href='http://www.strava.com/oauth/authorize?client_id=40250&response_type=code&redirect_uri=http://localhost:3000/user&scope=read,activity:read,activity:read_all&approval_prompt=force'> HERE</a> to Authorize us to use your Strava data</h1>
-        <br/>
+        <div className="text-right mb-3">
         <button class="btn btn-primary btn-lg" onClick={this.handleSubmit}>Click here to view your Bikes</button>
+        </div>
       </div>
     )
   }
