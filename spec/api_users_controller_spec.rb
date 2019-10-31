bcdd01262b5dd708f80347057c6eb6abd083245d
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-31 13:43:30 +0000

    Refactored test so that it doesn't have a before each

--- spec/api_users_controller_spec.rb
+++ spec/api_users_controller_spec.rb
@@ -1,11 +1,13 @@
-RSpec.describe Api::V1::UsersController, type: :request do
+# frozen_string_literal: true
 
-  describe "GET /users/index" do
-    before :each do
-      get "/api/v1/users/index"
+RSpec.describe Api::V1::UsersController, type: :request do
+  describe 'GET /users/index' do
+    # before :each do
+    #   get '/api/v1/users/'
+    # end
+    it 'responds with 200' do
+      get '/api/v1/users/'
+      expect(response).to have_http_status(200)
     end
-  it 'responds with 200' do
-    expect(response).to have_http_status(200)
   end
-end
 end
