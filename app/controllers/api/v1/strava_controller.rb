7693a019f6f1af36efeaf58c631c3236048b0251
Thomas Ross <thomasross3610@gmail.com>
2019-11-01 13:54:43 +0000

    authorize method in strava api controller refactored

--- app/controllers/api/v1/strava_controller.rb
+++ app/controllers/api/v1/strava_controller.rb
@@ -15,9 +15,10 @@ class Api::V1::StravaController < ApplicationController
   end
 
   def authorize
-    p "start"
-  response = RestClient.post 'https://www.strava.com/oauth/token', { client_id: '40250', client_secret: '700b02392cd20d926d066de6a28601acb90772a8', code: 'b3cd27673ccd4503e0c29be70b7d762260a8cd0e', grant_type: 'authorization_code' }
-  p JSON.parse(response)
+    code = params["code"]
+    response = RestClient.post 'https://www.strava.com/oauth/token', { client_id: '40250', client_secret: '700b02392cd20d926d066de6a28601acb90772a8', code: code, grant_type: 'authorization_code' }
+    json = JSON.parse(response)
+    p "Hello " + json['athlete']['firstname']
     redirect_to root_url
   end
 end
