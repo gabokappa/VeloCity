d81f29da3464673692fdedb8cc27ebffe3b27848
Gabriel Gonzalez <gabokappa@gmail.com>
2019-11-01 14:08:11 +0000

    Modified user model to have unique email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: true
+  validates_format_of :email, with: /@/
+  validates :password, presence: true
   has_many :bikes, dependent: :destroy
 end
