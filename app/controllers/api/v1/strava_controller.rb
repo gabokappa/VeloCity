406905c9795fae5e25ccc9a4a59dffe3d45ed26e
James Holton <james_holton@yahoo.co.uk>
2019-11-01 14:33:04 +0000

    Application link to Strava to exchange temp auth key, and updates User table

--- app/controllers/api/v1/strava_controller.rb
+++ app/controllers/api/v1/strava_controller.rb
@@ -15,10 +15,12 @@ class Api::V1::StravaController < ApplicationController
   end
 
   def authorize
-    code = params["code"]
-    response = RestClient.post 'https://www.strava.com/oauth/token', { client_id: '40250', client_secret: '700b02392cd20d926d066de6a28601acb90772a8', code: code, grant_type: 'authorization_code' }
+    response = RestClient.post 'https://www.strava.com/oauth/token', { client_id: '40250', client_secret: '700b02392cd20d926d066de6a28601acb90772a8', code: params["code"], grant_type: 'authorization_code' }
     json = JSON.parse(response)
-    p "Hello " + json['athlete']['firstname']
+    # TO DO
+    user = User.find_by(id: '1')
+    ########
+    user.update(strava_id: json['athlete']['id'], access_token: json['access_token'], access_token_expiry: Time.at(json['expires_at']), refresh_token: json['refresh_token'])
     redirect_to root_url
   end
 end
