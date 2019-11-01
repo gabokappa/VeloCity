82d47953e72a117fb68613e460d44e778d48a6d6
Gabriel Gonzalez <gabokappa@gmail.com>
2019-11-01 09:31:25 +0000

    SimpleCov configuration changes

--- spec/api_bikes_controller_spec.rb
+++ spec/api_bikes_controller_spec.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
+
 RSpec.describe Api::V1::BikesController, type: :request do
-  describe 'GET /bikes' do
+  describe 'GET #index' do
     it 'responds with 200' do
       get '/api/v1/bikes/index'
       expect(response).to have_http_status(200)
