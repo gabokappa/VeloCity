2828201440102b53edf7879ebccc64367f4925e4
karlo <deguzmankarlo@gmail.com>
2019-11-01 16:04:00 +0000

    Setup tokens controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: %i[show create update destroy]
+      resources :tokens, only: [:create]
+    end
+  end
+  # namespace :api do
+  #   namespace :v1 do
+  #     get 'tokens/create'
+  #   end
+  # end
   namespace :api do
     namespace :v1 do
       get 'components/index'
@@ -18,14 +29,6 @@ Rails.application.routes.draw do
       get 'bikes/destroy'
     end
   end
-
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      resources :users, only: %i[show create update destroy]
-  end
-    end
-
-
   # namespace :api do
   #   namespace :v1 do
   #     get 'users/index'
