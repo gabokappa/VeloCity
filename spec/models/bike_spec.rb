d932b157ab0f426fa745db1dc167cd8a09074b63
Gabriel Gonzalez <gabokappa@gmail.com>
2019-11-02 17:25:07 +0000

    Added Bike & Component model tests passing green

---
+++ spec/models/bike_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Bike, type: :model do
+  it 'has valid attributes' do
+    bike = FactoryBot.create(:bike)
+    expect(bike).to be_valid
+  end
+
+  it 'is not valid without a user id' do
+    bike = Bike.new(user_id: nil)
+    expect(bike.valid?).to eq(false)
+  end
+end
+
+# TODO Add tests here if the model changes and has another unique value , e.g strava id?
---
+++ spec/models/components_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Component, type: :model do
+  it 'has valid attributes' do
+    part = FactoryBot.create(:component)
+    expect(part).to be_valid
+  end
+
+  it 'is not valid without a bike_id' do
+    part = Component.new(bike_id: nil)
+    expect(part.valid?).to eq(false)
+  end
+
+end
