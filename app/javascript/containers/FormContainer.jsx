28fcab6ab5c86d3869783e1fa48ca9b0daf33e36
Gabriel Gonzalez <gabokappa@gmail.com>
2019-11-03 17:33:31 +0000

    Created a FormContainer for sign-up

---
+++ app/javascript/containers/FormContainer.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom"
+
+import CheckBox from '../components/CheckBox';
+import Input from '../components/Input';
+import Select from '../components/Select';
+import Button from '../components/Button';
+
+class FormContainer extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            newUser: {
+                first_name: '',
+                sur_name: '',
+                email: '',
+                password: ''
+
+            }
+        }
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.handleClearForm = this.handleClearForm.bind(this);
+    }
+    handleFormSubmit() {
+        // TODO: Add submision logic here
+    }
+
+    handleClearForm() {
+        // Logic for resetting the form
+    }
+
+    render() {
+        return (
+            <form className="container" onSubmit={this.handleFormSubmit}>
+                <Input /> {/* First Name */}
+                <Input /> {/*Sur Name*/}
+                <Input /> {/*Email*/}
+                <Input /> {/*Password*/}
+                <Button /> { /*Submit */}
+                <Button /> {/* Clear the form */}
+            </form>
+        );
+    }
+}
+export default FormContainer;
