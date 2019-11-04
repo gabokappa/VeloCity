8d7ef7412d023aece8e520fdae8ff06e7f95012d
James Holton <james_holton@yahoo.co.uk>
2019-11-04 12:24:21 +0000

    Renames password to password_digest on user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_30_174706) do
+ActiveRecord::Schema.define(version: 2019_11_04_121109) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,17 +38,16 @@ ActiveRecord::Schema.define(version: 2019_10_30_174706) do
     t.string "first_name"
     t.string "sur_name"
     t.string "username"
-    t.string "email", null: false
-    t.string "password_digest", null: false
+    t.string "email"
+    t.string "password_digest"
     t.integer "strava_id"
     t.string "access_token"
     t.datetime "access_token_expiry"
     t.string "refresh_token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "bikes", "users", on_delete: :cascade
-  add_foreign_key "components", "bikes", on_delete: :cascade
+  add_foreign_key "bikes", "users"
+  add_foreign_key "components", "bikes"
 end
