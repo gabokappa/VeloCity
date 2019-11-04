70f5c2965c92e86ea526d3f0ae02f9ddd297f464
Gabriel Gonzalez <gabokappa@gmail.com>
2019-11-04 13:49:46 +0000

    Signup component created Co-authored-by: Eduard Kulcsickij-Gut <eduard.kulcsickij.gut@gmail.com>

--- app/javascript/components/Signup.jsx
+++ app/javascript/components/Signup.jsx
@@ -4,28 +4,68 @@ import { Link } from "react-router-dom";
 class Signup extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {value: ''};
+        this.state = {
+            first_name: '',
+            sur_name: '',
+            email: '',
+            password: ''
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        this.setState({value: event.target.value});
+        const target = event.target;
+        const name = target.name;
+        const value = target.value
+        this.setState({[name]: value});
     }
 
     handleSubmit(event) {
-        alert('A name was submitted: ' + this.state.value);
+        alert('A name was submitted: ' + this.state.first_name);
         event.preventDefault();
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <form className="container py-1" onSubmit={this.handleSubmit}>
                 <label>
-                    Name:
-                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    First name:
+                    <input
+                        type="text"
+                        name="first_name"
+                        value={this.state.first_name}
+                        onChange={this.handleChange} />
                 </label>
+                <br />
+                <label>
+                    Surname:
+                    <input
+                        type="text"
+                        name="sur_name"
+                        value={this.state.sur_name}
+                        onChange={this.handleChange} />
+                </label>
+                <br />
+                <label>
+                    Email:
+                    <input
+                        type="text"
+                        name="email"
+                        value={this.state.email}
+                        onChange={this.handleChange} />
+                </label>
+                <br />
+                <label>
+                    Password:
+                    <input
+                        type="text"
+                        name="password"
+                        value={this.state.password}
+                        onChange={this.handleChange} />
+                </label>
+                <br />
                 <input type="submit" value="Submit" />
             </form>
         );
