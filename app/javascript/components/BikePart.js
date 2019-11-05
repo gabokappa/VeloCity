b31aa7fa58b8da60fee6df941a6b9464d5e355dd
Thomas Ross <thomasross3610@gmail.com>
2019-11-05 23:42:13 +0000

    %wear added to comoponents and warning colours added

--- app/javascript/components/BikePart.js
+++ app/javascript/components/BikePart.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
 
 class BikePart extends Component {
-  
+
   render() {
     const { part } = this.props
+    function percentWorn(part) {
+      return (( part.distance_done / part.max_distance ) * 100).toFixed()
+    }
+
+function wornColour(part) {
+      console.log("called")
+      if(percentWorn(part) < 75) {
+        return {backgroundColor: "green"}
+      }
+      else if(percentWorn(part) < 100) {
+        return {backgroundColor: "orange"}
+      }
+      else {
+        return {backgroundColor: "red"}
+    };
+  };
+
     return (
-      <div>Component: {part.comp_name} ({part.distance_done} / {part.max_distance})</div>
+      <div style={wornColour(part)}>Component: {part.comp_name} <br/>
+      Distance done:{part.distance_done}m <br/>
+      Recommended maximum: {part.max_distance}m <br/>
+      Percentage worn: { percentWorn(part) }%</div>
     )
   }
 }
