3c7380d55aa93147e399739ff6de9b0bc58f6aec
karlo <deguzmankarlo@gmail.com>
2019-11-05 17:19:19 +0000

    State driven login and logout buttons in heading - tweaking required

--- app/javascript/components/Heading.js
+++ app/javascript/components/Heading.js
@@ -7,19 +7,37 @@ class Heading extends Component {
   constructor(props) {
     super(props);
     this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.state = {isLoggedIn: false}
+  }
 
+  handleLoginClick() {
+    this.setState({isLoggedIn: true})
+    .then(() => {
+      this.props.history.push("/login")
+    })
   }
 
   handleLogoutClick() {
     ls.remove('authorization')
     ls.remove('user_id')
+    this.setState({isLoggedIn: false})
   }
 
   render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+
+    if (isLoggedIn) {
+      button = <div> <button className="btn btn-primary btn-lg" onClick={this.handleLogoutClick}>Logout</button></div>
+    } else {
+      button = <Link to="/login" className="btn btn-link" onClick={this.handleLoginClick}>Login</Link>
+    }
+
     return (
       <div className="jumbotron  p-1">
         <Link to="/" className="btn btn-link">Home</Link>
-        <div> <button className="btn btn-primary btn-lg" onClick={this.handleLogoutClick}>Logout</button></div>
+        {button}
         <h1 className="display-1 text-center">
           <img className="img-thumbnail" src='https://myext.info/image/icon_large/kdemfmhkoncmbcphejembfngiihppkei.jpg'/>
           VeloCity Application
