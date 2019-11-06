1fff8ccfc1c0eb0bcb5cc3311ecc5ad863e6974a
James Holton <james_holton@yahoo.co.uk>
2019-11-06 14:07:52 +0000

    Fix relative position of header

--- app/javascript/components/Heading.js
+++ app/javascript/components/Heading.js
@@ -15,7 +15,7 @@ class Heading extends Component {
 
   render() {
     return (
-      <div className="jumbotron  p-1 position-sticky">
+      <div className="jumbotron  p-1 sticky">
         <Link to="/bikes" className="btn btn-link">Home</Link>
         <Link to="/signup" className="btn btn-link">Sign Up</Link>
         <Link to="/login" className="btn btn-link">Login</Link>
