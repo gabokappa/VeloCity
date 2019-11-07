35e54a15c56b4a40548310a71d64290945d341e4
James Holton <james_holton@yahoo.co.uk>
2019-11-07 10:23:47 +0000

    Added logon error to login scree.....again

--- app/javascript/components/Login.jsx
+++ app/javascript/components/Login.jsx
@@ -1,50 +1,83 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import ls from 'local-storage';
-import Image from './bicycle-breaks-professional-6946.jpg';
+import React from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-// import Link from '@material-ui/core/Link';
+import Link from '@material-ui/core/Link';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Image from './bicycle-breaks-professional-6946.jpg';
+import ls from 'local-storage';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        height: '100vh',
+    },
+    image: {
+        backgroundImage: `url(${Image})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 
-class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: '',
-            token: 'test'
-        };
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
 
-    handleChange(event) {
-        const target = event.target;
-        const name = target.name;
-        const value = target.value;
-        this.setState({[name]: value});
-    }
+export default function SignInSide() {
 
-    handleSubmit(event) {
+    const handleSubmit = () => {
         event.preventDefault();
-        let userData = this.state;
+        console.log(document.getElementById("email").value);
         const body_test = JSON.stringify({"user": {
-                "email": userData.email,
-                "password": userData.password
+                "email": document.getElementById("email").value,
+                "password": document.getElementById("password").value
             }
         })
+
+    const handleErrors = (response) => {
+        if (!response.ok) {
+            alert("Your login details are incorrect, please try again.");
+        }
+        return response;
+    }
+
         const url = "api/v1/tokens";
         let textStuff = ""
         fetch(url, {
@@ -54,49 +87,82 @@ class Login extends React.Component {
                 'Accept': "application/json",
                 "Content-Type": "application/json"
             }
-        }).then(response => response.text())
-          .then(json => {
+        })  .then(handleErrors)
+            .then(response => response.text())
+            .then(json => {
                 ls.set('authorization', JSON.parse(json).token)
                 ls.set('user_id', JSON.parse(json).user_id)
           })
-          .then(redirect => {this.props.history.push('/bikes')})
-        ;
+          .then(redirect => window.location.replace("/bikes"));
     }
+    const classes = useStyles();
 
-
-    render() {
-        return (
-          <div>
-              <Link to="/signup" className="btn btn-link">New to VeloCity? Register here</Link>
-              <div>
-            <form className="form-group ml-5 px-2"onSubmit={this.handleSubmit}>
-                <div className="form-group">
-                <label>
-                    Email:
-                    <input className="form-control"
-                        type="email"
-                        name="email"
-                        value={this.state.email}
-                        onChange={this.handleChange} />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input className="form-control"
-                        type="password"
-                        name="password"
-                        value={this.state.password}
-                        onChange={this.handleChange} />
-                </label>
-                <br />
+    return (
+        <Grid container component="main" className={classes.root}>
+            <CssBaseline />
+            <Grid item xs={false} sm={4} md={7} className={classes.image} />
+            <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form onSubmit={handleSubmit} className={classes.form} noValidate>
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                        <Box mt={5}>
+                            <Copyright />
+                        </Box>
+                    </form>
                 </div>
-                <input className="btn btn-success" type="submit" value="Login" />
-            </form>
-              </div><div><img className="d-lg-inline" src={Image} alt={"welcome image"} /></div>
-          </div>
-
-        );
-    }
+            </Grid>
+        </Grid>
+    );
 }
-
-export default Login;
