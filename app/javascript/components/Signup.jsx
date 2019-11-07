5ef27353c029699212cdbd1a30f18242c0536328
Thomas Ross <thomasross3610@gmail.com>
2019-11-07 14:14:45 +0000

    sign up css ammended to be the same as login

--- app/javascript/components/Signup.jsx
+++ app/javascript/components/Signup.jsx
@@ -1,97 +1,172 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Redirect } from "react-router-dom";
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import ls from 'local-storage';
 
-class Signup extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            first_name: '',
-            sur_name: '',
-            email: '',
-            password: ''
-        };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    handleChange(event) {
-        const target = event.target;
-        const name = target.name;
-        const value = target.value;
-        this.setState({[name]: value});
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        let userData = this.state;
-        const url = "api/v1/signup/create";
-        fetch(url, {
-            method: "POST",
-            body: JSON.stringify(userData),
-            headers: {
-                'Accept': "application/json",
-                "Content-Type": "application/json"
-            }
-        }).then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error("Network response was not ok.");
-        })
-            .then(json => {
-              ls.set('user_id', json.id)
-            })
-            .then(reload => window.location.reload())
-    }
-
-    render() {
-        return (
-            <form className="form-group ml-5 px-2" onSubmit={this.handleSubmit}>
-                <h2>Step 1/2 - Register for VeloCity</h2>
-                <div className="form-group">
-                    <label> First name:
-                        <input className="form-control"
-                               type="text"
-                               name="first_name"
-                               value={this.state.first_name}
-                               onChange={this.handleChange} />
-                    </label>
-                    <br />
-                    <label>
-                        Surname:
-                        <input className="form-control"
-                               type="text"
-                               name="sur_name"
-                               value={this.state.sur_name}
-                               onChange={this.handleChange} />
-                    </label>
-                    <br />
-                    <label>
-                        Email:
-                        <input className="form-control"
-                               type="email"
-                               name="email"
-                               value={this.state.email}
-                               onChange={this.handleChange} />
-                    </label>
-                    <br />
-                    <label>
-                        Password:
-                        <input className="form-control"
-                               type="password"
-                               name="password"
-                               value={this.state.password}
-                               onChange={this.handleChange} />
-                    </label>
-                    <br />
-                </div>
-                <input className="btn btn-success" type="submit" value="Register" />
-            </form>
-        );
-    }
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
 }
 
-export default Signup;
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignUp() {
+  const classes = useStyles();
+
+  const handleSubmit = (event) => {
+      event.preventDefault();
+      const url = "api/v1/signup/create";
+      const userData = JSON.stringify( { "first_name": document.getElementById("firstName").value,
+      "sur_name": document.getElementById("lastName").value, "email": document.getElementById("email").value,
+      "password": document.getElementById("password").value } )
+
+      fetch(url, {
+          method: "POST",
+          body: userData,
+          headers: {
+              'Accept': "application/json",
+              "Content-Type": "application/json"
+          }
+      }).then(response => {
+          if (response.ok) {
+              return response.json();
+          }
+          throw new Error("Network response was not ok.");
+      })
+          .then(json => {
+            ls.set('user_id', json.id)
+          })
+          .then(reload => window.location.reload())
+  }
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <form onSubmit={handleSubmit} className={classes.form} noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                label="I want to receive inspiration, marketing promotions and updates via email."
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link href="#" variant="body2">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={5}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
